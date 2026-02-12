547822cccd4e09ed50b81c81e2028f9d72f3199a
OOjas Vinod Joshi <ojas7joshi@gmail.com>
2026-02-12 18:53:14 +0530

    connect to production backend

--- founders-faceoff/client/src/socket.js
+++ founders-faceoff/client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 console.log("Connecting to backend...");
 
-const socket = io("http://127.0.0.1:5000");
+const socket = io("https://founders-faceoff.onrender.com");
 
 socket.on("connect", () => {
   console.log("Connected to backend:", socket.id);
